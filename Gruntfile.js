e3a6e76be2b3d2ba711f38fd949bd8c5e318bcb5
Tristan Teufel <tristanteufel@googlemail.com>
2015-01-25 22:31:56 +0100

    code style formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,5 @@
-/**
- * Copyright 2013, 2014 IBM Corp.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
 module.exports = function(grunt) {
 
-    // Project configuration.
     grunt.initConfig({
 	pkg : grunt.file.readJSON('package.json'),
 	simplemocha : {
@@ -33,23 +16,17 @@ module.exports = function(grunt) {
 	},
 	jshint : {
 	    options : {
-		// http://www.jshint.com/docs/options/
-		"asi" : true, // allow missing semicolons
-		"curly" : true, // require braces
-		"eqnull" : true, // ignore ==null
-		"forin" : true, // require property filtering in "for in" loops
-		"immed" : true, // require immediate functions to be wrapped in
-				// ( )
-		"nonbsp" : true, // warn on unexpected whitespace breaking
-				    // chars
-		// "strict": true, // commented out for now as it causes 100s of
-		// warnings, but want to get there eventually
-		"loopfunc" : true, // allow functions to be defined in loops
+		"asi" : true,
+		"curly" : true,
+		"eqnull" : true,
+		"forin" : true,
+		"immed" : true,
+		"nonbsp" : true,
+		"loopfunc" : true,
 		"sub" : true
-	    // don't warn that foo['bar'] should be written as foo.bar
 	    },
 	    all : {
-		src : [ 'wifi370.js' ],
+		src : [ 'homematic.js' ],
 	    },
 	},
 	inlinelint : {
